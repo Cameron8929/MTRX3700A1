d4b0c6e62a0a401020bd6f97b9029ce929439f1b
Cameron8929 <160560741+Cameron8929@users.noreply.github.com>
2024-08-31 16:26:26 +1000

    Update rng
    Update rng

--- rng.v
+++ rng.v
@@ -5,14 +5,15 @@ module rng #(
     input clk,
     output [$clog2(MAX_VALUE)-1:0] random_value // 5-bits for values 0 to 32.
 );
-    reg [10:1] lfsr; // The 10-bit Linear Feedback Shift Register. Note the 10 down-to 1. (No bit-0, we count from 1 in this case!)
+    reg [$clog2(MAX_VALUE):1] lfsr; // The 10-bit Linear Feedback Shift Register. Note the 10 down-to 1. (No bit-0, we count from 1 in this case!)
 
     // Initialise the shift reg to SEED, which should be a non-zero value:
     initial lfsr = SEED;
 
     // Set the feedback:
     wire feedback;
-    assign feedback = lfsr[10] ^ lfsr[7];
+//    assign feedback = lfsr[10] ^ lfsr[7];
+	 assign feedback = lfsr[$clog2(MAX_VALUE)] ^ lfsr[$clog2(MAX_VALUE)-2];
 
     //    Make sure to shift left from bit 1 (LSB) towards bit 10 (MSB).
     always @(posedge clk)
@@ -22,5 +23,6 @@ module rng #(
     end
 
     // Ensure that the random value is not outside of the LED range.
-    assign random_value = (lfsr[5:1]>MAX_VALUE) ? (lfsr[5:1] - MAX_VALUE) : lfsr[5:1];
+//    assign random_value = (lfsr>MAX_VALUE) ? (lfsr - MAX_VALUE) : lfsr;
+	 assign random_value = lfsr % (MAX_VALUE + 1);
 endmodule
